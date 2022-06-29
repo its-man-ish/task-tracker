cdf019b1bf46d7157303000e2dddea27b473d019
its-man-ish <manishbsn188@gmail.com>
2022-06-29 22:29:01 +0530

    event

--- blogs/src/Home.js
+++ blogs/src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick =()=>{
+        console.log("Hellow Bro")
+    }
     return (
       <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click</button>
       </div>
     );
   }
