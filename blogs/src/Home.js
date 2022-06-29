13f557016fff2937a2ff59b7a960cdfb8e213fd0
its-man-ish <manishbsn188@gmail.com>
2022-06-29 16:24:27 +0530

    day 1

---
+++ blogs/src/Home.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar';
+
+const Home = ()=>{
+    return(
+        <div className="home">
+            Home
+        <Navbar />
+        </div>
+    )
+}
+
+export default Home;
---
+++ blogs/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = ()=>{
+    return (
+        <nav>
+            <h1>MK Blogs</h1>
+            <div className="links">
+                <a href="">Home</a>
+                <a href="/creat">New Blogs</a>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar;
