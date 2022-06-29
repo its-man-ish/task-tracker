f60fab39233ef7e1b663f8b813ffe1ea6ce740e4
its-man-ish <manishbsn188@gmail.com>
2022-06-29 22:31:55 +0530

    event with param

--- blogs/src/Home.js
+++ blogs/src/Home.js
@@ -2,10 +2,17 @@ const Home = () => {
     const handleClick =()=>{
         console.log("Hellow Bro")
     }
+    const handleClickAgain =(name)=>{
+        console.log("Hellow Bro",name)
+    }
     return (
       <div className="home">
         <h2>Homepage</h2>
         <button onClick={handleClick}>Click</button>
+        <button onClick={()=>{
+            handleClickAgain("Manish")
+        }}>Click2</button>
+
       </div>
     );
   }
