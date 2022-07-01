03afac99640bfb6624f65fcc03579cbf42c5b2a3
its-man-ish <manishbsn188@gmail.com>
2022-07-02 02:36:09 +0530

    fatch data from api using useEffect

--- blogs/src/Home.js
+++ blogs/src/Home.js
@@ -2,21 +2,7 @@
 import { useState, useEffect } from "react"
 import BlogList from "./Blogs"
 const Home = () => {
-    const [name,setName] = useState("Manish")
-    const [blogs,setBlogs]=useState([
-         {
-        title:"Naruto Uzumaki", 
-        body:"Guided by the spirit demon within him, Naruto learns to harness his powers as a ninja in this anime adventure series.",
-        author:"Masashi Kishimoto",
-        id:1,
-       },
-       {
-        title:"Death Note", 
-        body:"The story follows Light Yagami, a teen genius who discovers a mysterious notebook: the Death NOte.",
-        author:"Tsugumi Ohba ",
-        id:2,
-       }
-    ]);
+    const [blogs,setBlogs]=useState(null);
    
     const deleteBlog= (id)=>{
       const  newBlogs = blogs.filter(blog=>blog.id!==id);
@@ -24,14 +10,22 @@ const Home = () => {
     }  
   
     useEffect(()=>{
-      console.log('use effect ran')
-      console.log(blogs)
-    },[name])
+       fetch('http://localhost:8000/blogs')
+       .then(res=>{
+         return res.json();
+       })
+       .then((data)=>{
+         console.log(data)
+         setBlogs(data)
+       })
+    },[])
+
     return (
       <div className="home">
-        <BlogList blogs = {blogs} deleteBlog={deleteBlog} />
-        {name}
-        <button onClick={()=>setName("Chotu")}>Name</button>
+      {
+         blogs &&     <BlogList blogs = {blogs} deleteBlog={deleteBlog} />
+
+      }     
       </div>
     );
   }
