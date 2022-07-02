d20e96d7b5a66e4633873c47e519c415923a292e
its-man-ish <manishbsn188@gmail.com>
2022-07-02 09:29:07 +0530

    useEffect complete

--- blogs/src/Home.js
+++ blogs/src/Home.js
@@ -4,26 +4,38 @@ import BlogList from "./Blogs"
 const Home = () => {
     const [blogs,setBlogs]=useState(null);
     const [isPending,setisPending] = useState(true);
+    const [err,setError] = useState(null) 
    
     const deleteBlog= (id)=>{
       const  newBlogs = blogs.filter(blog=>blog.id!==id);
-      setBlogs(newBlogs)
+      setBlogs(newBlogs);
     }  
   
     useEffect(()=>{
        fetch('http://localhost:8000/blogs')
        .then(res=>{
+         if (!res.ok) {
+           throw Error("ERROR 404 NOT FOUND");
+         }
          return res.json();
        })
        .then((data)=>{
          console.log(data);
          setBlogs(data);
          setisPending(false);
+         setError(null);
+       })
+       .catch(err=>{
+         setisPending(false);
+         setBlogs(null);
+         setError(err.message);
        })
     },[])
 
     return (
       <div className="home">
+
+      { err && <div>{err}</div>}
 
       { isPending && <div>Loading...</div>}
       {
