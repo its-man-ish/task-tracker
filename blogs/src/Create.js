560b933e574dac3547f4f970beafa7d35fb75ee4
its-man-ish <manishbsn188@gmail.com>
2022-07-03 16:27:02 +0530

    forms and data binding

--- blogs/src/Create.js
+++ blogs/src/Create.js
@@ -1,7 +1,48 @@
+import {useState } from 'react'
 const Create = ()=>{
+   const [title,setTitle] = useState('');
+   const [body,setBody] = useState('');
+   const [genre,setGenre] = useState('');
+
+
+
     return(
         <div className="create">
           <h3>Create a New Bog </h3>
+
+          <form action="">
+             <label htmlFor="">Blog title</label>
+
+             <input 
+             value={title}
+             onChange={(e)=>setTitle(e.target.value)}
+             type="text"
+             required
+              />
+              <label htmlFor="">Genre</label>
+              <select name="" id="" 
+               value={genre}
+               onChange={(e)=>setGenre(e.target.value)}
+              required
+              >
+                <option value="game">Game</option>
+                <option value="fashion">Fashion</option>
+                <option value="anime">Anime</option>
+                <option value="education">Education</option>
+                <option value="Technology">Technology</option>
+                <option value="Politics">Politics</option>
+              </select>
+            <label htmlFor="">Blog body</label>
+            <textarea 
+            value={body}
+            onChange={(e)=>setBody(e.target.value)}
+            required
+            ></textarea>
+            <button>Add Blog</button> <br />
+
+            
+
+          </form>
         </div>
     )
 }
--- blogs/src/Detail.js
+++ blogs/src/Detail.js
@@ -1,11 +1,25 @@
 import { useParams }from 'react-router-dom'
+
+import useFetch from './useFetch';
 const BlogDetails = ()=>{
 
     const {id} = useParams();
-
+    const {data:blog, error, isPending } = useFetch('http://localhost:8000/blogs/'+id);
+    console.log("data: ",blog)
     return(
         <div className="blog-details">
-            <h2>Blog Detail- {id}</h2> 
+          {isPending && <div>Loading...</div>}
+          {error && <div>{error}</div> }
+          {blog && (
+              <article>
+                  <h2>{blog.title}</h2>
+                  <p>written by {blog.author}</p>
+                  <hr/>
+                  <div>
+                      {blog.body}
+                  </div>
+              </article>
+          )}
         </div>
     );
 }
